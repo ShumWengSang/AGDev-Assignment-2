3052bfc27b8aaab555f2a3d1584ab4e197eda59b
ShumWengSang <ballwallcallall@gmail.com>
2015-02-06 14:14:21 +0800

    lua debug file

---
+++ Main/lua+dll/Debug.lua
@@ -0,0 +1,11 @@
+local Camz, Camy, Camx, FPS = GetCamAndFPS()
+
+PrintOnScreen(10, 100, "Camera 2 Pos: ", Camx, " ", Camy, " ", Camz, " ")
+PrintOnScreen(10,50, "FPS: ", FPS)
+
+print("START")
+print(FPS)
+print(Camz)
+print(Camy)
+print(camx)
+print("END")
