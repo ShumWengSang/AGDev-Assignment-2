f25de26edbe74a1071da3a49d2d38e5756e44d86
ShumWengSang <ballwallcallall@gmail.com>
2015-02-06 15:36:15 +0800

    update lia

--- Main/lua+dll/Init.lua
+++ Main/lua+dll/Init.lua
@@ -4,3 +4,21 @@ ScreenHeight = 600
 fov = 60
 MazeWidth = 20
 MazeHeight = 20
+PCamx, PCamy, PCamz = 10,10,10
+DCamx, DCamy, DCamz = 0, 0, 1
+
+front = "SkyBox/front.tga"
+back = "SkyBox/back.tga"
+left = "SkyBox/left.tga"
+right = "SkyBox/right.tga"
+top = "SkyBox/top.tga"
+bottom = "SkyBox/bottom.tga"
+
+obj = "Objects/Cube.obj"
+
+Top = 100
+Bottom = -50
+Left = 100
+Right = -100
+Near = -100
+Far = 100
